eab513eccf6b43e69a2123b25ba5c4d171335fc5
18520945 <18520945@gm.uit.edu.vn>
2022-07-07 17:21:36 +0700

    add some file

---
+++ ubuntu20-awscli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo apt-get update
+sudo apt install nginx -y
